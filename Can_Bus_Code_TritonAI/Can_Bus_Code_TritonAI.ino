6df4a01bd553e5e755f73e19e380896a10dc5ced
Justin Chou <justintchou@gmail.com>
2023-05-11 18:23:44 -0700

    cleaning

--- Can_Bus_Code_TritonAI/Can_Bus_Code_TritonAI.ino
+++ Can_Bus_Code_TritonAI/Can_Bus_Code_TritonAI.ino
@@ -5,12 +5,13 @@
 #include <SPI.h>
 #include "mcp_can.h"
 
+const int heartbeatseizuretime = 100 //ms
 
 const int SPI_CS_PIN = 17;  //Can Bus CS pin using LONGAN library
-int pin8 = 8;   //Initialize pins for sending signals to the relay
-int pin9 = 9;
-int pin10 = 10;
-int pin11 = 11;
+int RED_RELAY_PIN = 8;   //Initialize pins for sending signals to the relay
+int YELLOW_RELAY_PIN = 9;
+int GREEN_RELAY_PIN = 10;
+int BUZZER_RELAY_PIN = 11;
 
 String readString;    //Initialize strings for reading Can-Bus signals
 String color;
@@ -22,12 +23,8 @@ int dash2;
 int dash3;
 
 unsigned long timer = millis();   //Initialize variables for time dependent color patterns
-unsigned long flag1 = 0;          //Flag for Buzzer
-unsigned long flag2 = 0;
 static unsigned long heartHold = 250;
 long heartTimer = 0;
-long colorTimer = 0;
-long counter = 0;
 long interval = 0;
 int division = 0;
 
@@ -35,11 +32,13 @@ unsigned long redbefore = millis();
 unsigned long yellowbefore = millis();
 unsigned long greenbefore = millis();
 unsigned long buzzerbefore = millis();
+unsigned long rotatebefore = millis();
 
 unsigned int redcount = 0;
 unsigned int yellowcount = 0;
 unsigned int greencount = 0;
 unsigned int buzzercount = 0;
+unsigned int rotatecount = 0;
 
 //Total runtime of the process. Used for timer purposes
 
@@ -59,10 +58,10 @@ void setup() {
 
 
   // Define pins as outputs
-  pinMode(pin8, OUTPUT);
-  pinMode(pin9, OUTPUT);
-  pinMode(pin10, OUTPUT);
-  pinMode(pin11, OUTPUT);
+  pinMode(RED_RELAY_PIN, OUTPUT);
+  pinMode(YELLOW_RELAY_PIN, OUTPUT);
+  pinMode(GREEN_RELAY_PIN, OUTPUT);
+  pinMode(BUZZER_RELAY_PIN, OUTPUT);
 
   WatchDog::init(OVF_500MS);
 }
@@ -83,75 +82,18 @@ void loop() {
 
     unsigned long canId = CAN.getCanId();
 
-    Serial.println("-----------------------------");   //Give metadata/ID info about CAN signal
-    Serial.print("Get data from ID: ");
-    Serial.println(canId, HEX);
-
-    for (int i = 0; i < len; i++) { //print the data
-      Serial.print(buf[i], HEX);
-      Serial.print("\t");
-    }
-
-    counter = timer;   //Set "counter" to the time when a signal is received
     heartTimer = timer; //Update heartTimer whenever a CAN signal is received
   }
+  timer = millis(); //Update timer
+  checkHeartTimer();
 
-  String readString = buf;   //Turn CAN message into readable stirng
-
-  // Parse Can-Bus data into workable Strings
-  //Data expected in Color-Time-Repetition
-  dash1 = readString.indexOf('-');  //finds location of first '-'
-  color = readString.substring(0, dash1);   //captures first data String
-  dash2 = readString.indexOf('-', dash1 + 1); //finds location of second '-'
-  timeInterval = readString.substring(dash1 + 1, dash2); //captures second data String
-  count = readString.substring(dash2 + 1); //captures remain part of data after last '-'
-
-  // Print the parts of the message just to make sure everything is received properly
-  /*
-    Serial.print("color = ");
-    Serial.println(color);
-    Serial.print("hold time = ");
-    Serial.println(timeInterval);
-    Serial.print("number of times = ");
-    Serial.println(count);
-  */
-
-  // Heartbeat conditions
-  flag1 = (timer - heartTimer) >= heartHold;
-  interval = timer - counter;
-  division = interval / heartHold;
-
-  if (division % 2 == 0) && (flag1 == 1) {
-    digitalWrite(pin10, HIGH)
+  boolean redcond = false; //example of a condition for a color signal
+  if(redcond) {
+    redcount = 5;
+    red_relay_operation(heartbeatseizuretime, redcount);
   }
-
-  else if (division % 2 == 1) && (flag1 == 1) {
-    digitalWrite(pin10, LOW)
-  }
-
-
   // Color signal conditions
 
-  if (timeInterval == "1") {
-    digitalWrite(pin11, HIGH);
-  }
-
-  else if (timeInterval == "0") {
-    digitalWrite(pin11, LOW);
-  }
-
-  else {
-    flag2 = (timer - colorTimer) >= timeInterval;
-    for i = 1 : count {
-      if (color == "buzzer") && (flag2 == 1) {
-            digitalWrite(pin11, HIGH);
-      }
-
-      else if (color == "buzzer") && (flag2 == 0) {
-            digitalWrite(pin11, LOW);
-      }
-    }
-  }
 }
 
 void red_relay_operation(int duration, int repetitions) {
@@ -163,7 +105,7 @@ void red_relay_operation(int duration, int repetitions) {
       return;
     }
     digitalWrite(RED_RELAY_PIN, !digitalRead(RED_RELAY_PIN));
-    greenbefore = now;
+    redbefore = now;
     count = count - 1;
     //Serial.print(count);
   }
@@ -178,7 +120,7 @@ void yellow_relay_operation(int duration, int repetitions) {
       return;
     }
     digitalWrite(YELLOW_RELAY_PIN, !digitalRead(YELLOW_RELAY_PIN));
-    greenbefore = now;
+    yellowbefore = now;
     count = count - 1;
     //Serial.print(count);
   }
@@ -201,15 +143,24 @@ void green_relay_operation(int duration, int repetitions) {
 
 void buzzer_relay_operation(int duration, int repetitions) {
   unsigned long now = millis();
-  if(now-greenbefore >= duration) {
+  if(now-buzzerbefore >= duration) {
     //Serial.print("Here" + repetitions-1);
     if(count == 0) {
       digitalWrite(BUZZER_RELAY_PIN, LOW);
       return;
     }
     digitalWrite(BUZZER_RELAY_PIN, !digitalRead(BUZZER_RELAY_PIN));
-    greenbefore = now;
+    buzzerbefore = now;
     count = count - 1;
     //Serial.print(count);
+  }
+}
+
+void checkHeartBeat() {
+  if(timer - heartTimer >= heartHold) {
+    redcount = 1;
+    red_relay_operation(heartbeatseizuretime, redcount);
+  } else {
+    redcount = 0;
   }
 }
